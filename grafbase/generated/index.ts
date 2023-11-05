a5e946fcc5c47287404936fa383e15bcba75e8bd
varunnlv <varun.netinti@gmail.com>
2023-11-05 13:58:53 +0530

    Tenth

---
+++ grafbase/generated/index.ts
@@ -0,0 +1,28 @@
+// This is a generated file. It should not be edited manually.
+//
+// You can decide to commit this file or add it to your `.gitignore`.
+//
+// By convention, this module is imported as `@grafbase/generated`. To make this syntax possible,
+// add a `paths` entry to your `tsconfig.json`.
+//
+//  "compilerOptions": {
+//    "paths": {
+//      "@grafbase/generated": ["./grafbase/generated"]
+//    }
+//  }
+
+export type Schema = {
+  'User': {
+    __typename?: 'User';
+    name: string;
+    email: string;
+    description: string | null;
+    projects?: Array<Schema['Project']> | null;
+  };
+  'Project': {
+    __typename?: 'Project';
+    title: string;
+    description: string;
+    createdBy?: Schema['User'];
+  };
+};
