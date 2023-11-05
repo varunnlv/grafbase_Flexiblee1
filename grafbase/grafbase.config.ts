a48e7f5994eb9f73092b55c83261faea1cfc45eb
varunnlv <varun.netinti@gmail.com>
2023-11-05 14:27:39 +0530

    Thirteen

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -27,33 +27,31 @@ const address = g.type('Address', {
 })
 
 
-const createUserModel = () => {
-  return mongo.model('User', {
+const User = mongo
+  .model('User', {
     name: g.string(),
     email: g.string().optional(),
     address: g.ref(address),
     avatarUrl: g.url(),
     description: g.string().length({ min: 2, max: 1000 }).optional(),
     githubUrl: g.url().optional(),
-    linkedinUrl: g.url().optional(),
-    projects: g.relation(createProjectModel).list().optional() as any,
-  }).collection('users');
-};
+    linkedinUrl: g.url().optional(), 
+    //projects: g.relation(() => Project).list().optional() as any,
+  })
+  .collection('users')
 
-const createProjectModel = () => {
-  return mongo.model('Project', {
-    title: g.string().length({ min: 3 }),
-    description: g.string(),
-    image: g.url(),
-    liveSiteUrl: g.url(),
-    githubUrl: g.url(),
-    createdBy: g.relation(createUserModel).list().optional() as any,
-  }).collection('projects');
-};
 
-// Initialize models
-const User = createUserModel();
-const Project = createProjectModel();
+
+// // // @ts-ignore
+const Project = mongo.model('Project', {
+  title: g.string().length({ min: 3 }),
+  description: g.string(), 
+  image: g.url(),
+  liveSiteUrl: g.url(), 
+  githubUrl: g.url(), 
+  //category: g.string().search(),
+  ///createdBy: g.relation(() => User).list().optional() as any,
+}).collection('projects')
 
 // @ts-ignore
 // const User = mongo.model('User', {
